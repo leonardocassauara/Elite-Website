03a0727d3e889c6738a763239802210970a90e7d
Leonardo Cassauara <leomaia.rj@hotmail.com>
2024-04-26 21:35:26 -0300

    feat: criando componente ícone

---
+++ components/Icon/Icon.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+type AppProps = {
+  path: string;
+  width: number;
+  height: number;
+  alt: string;
+};
+
+export default function Icon({ path, width, height, alt }: AppProps) {
+  return (
+    <>
+      <Image
+        src={"/img/" + path + ".svg"}
+        width={width}
+        height={height}
+        alt={alt}
+      />
+    </>
+  );
+}
