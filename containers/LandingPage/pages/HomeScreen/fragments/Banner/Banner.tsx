d034c3111adb0a3a97cdf35c5ea7e54080451e82
Leonardo Cassauara <leomaia.rj@hotmail.com>
2024-05-01 12:18:54 -0300

    feat: criando fragment banner

---
+++ containers/LandingPage/pages/HomeScreen/fragments/Banner/Banner.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import styles from "./Banner.module.css";
+
+export default function Banner() {
+  return (
+    <>
+      <div className={styles["background-container"]}>
+        <div className={styles["content-container"]}>
+          <Image
+            src="/img/banner.png"
+            width={1200}
+            height={360}
+            className={styles["banner"]}
+            alt="Banner promocional"
+            sizes="(max-width: 768px) 768px, (maxwidth: 1024px) 1024px, 100%"
+            priority
+          />
+        </div>
+      </div>
+    </>
+  );
+}
