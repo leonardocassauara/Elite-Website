8ffb8c3e7423d0babfb50e3917b549671f1d6a2c
Leonardo Cassauara <leomaia.rj@hotmail.com>
2024-05-04 11:26:28 -0300

    feat: criando componente cartão de produto

---
+++ components/ProductCard/ProductCard.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./ProductCard.module.css";
+
+type AppProps = {
+  id: string;
+  name: string;
+  price: string;
+};
+
+export default function ProductCard({ id, name, price }: AppProps) {
+  return (
+    <>
+      <div id={id} className={styles["main-container"]}>
+        <Link href="./">
+          <Image
+            src="/img/product.png"
+            width={318}
+            height={345}
+            alt="Produto em destaque"
+            className="image"
+          />
+          <div className={styles["text-container"]}>
+            <div className={styles["content-container"]}>
+              <h3 className={styles["title"]}>{name}</h3>
+              <p className={styles["description"]}>R${price}</p>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </>
+  );
+}
