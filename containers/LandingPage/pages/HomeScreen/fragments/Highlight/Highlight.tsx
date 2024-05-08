f6c4070fd54648595b55c5191ec35d6d7621d179
Leonardo Cassauara <leomaia.rj@hotmail.com>
2024-05-08 15:29:50 -0300

    feat: criando componente destaque

---
+++ containers/LandingPage/pages/HomeScreen/fragments/Highlight/Highlight.tsx
@@ -0,0 +1,37 @@
+import Title from "@/components/Title/Title";
+import styles from "./Highlight.module.css";
+import ProductCard from "@/components/ProductCard/ProductCard";
+
+// TODO: Substituir mock por um read do realtime database do Firebase
+
+export default function Highlight() {
+  // * Mock
+  const data = [
+    { id: "1", name: "Pneu Dunlop", price: "199" },
+    { id: "2", name: "Pneu Strong", price: "199" },
+    { id: "3", name: "Motor Aud", price: "1240" },
+    { id: "4", name: "Motor BMW", price: "1240" },
+    { id: "5", name: "Pneu Remold 150 60 87", price: "1240" },
+  ];
+
+  return (
+    <>
+      <div
+        className={`${styles["background-container"]} + " " + ${styles["top"]}`}
+      >
+        <div className={styles["title-container"]}>
+          <Title text="PROMOÇÕES" color="#000000" pseudo />
+        </div>
+      </div>
+      <div
+        className={`${styles["background-container"]} + " " + ${styles["bot"]}`}
+      >
+        <div className={styles["card-container"]}>
+          {data.map(({ id, name, price }) => {
+            return <ProductCard key={id} id={id} name={name} price={price} />;
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
