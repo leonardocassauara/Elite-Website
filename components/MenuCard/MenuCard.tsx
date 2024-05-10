18ab6e5c55d4779af9cb2a7b2075ade8c9d38216
Leonardo Cassauara <leomaia.rj@hotmail.com>
2024-05-10 13:58:50 -0300

    feat: criando componente cartão menu

---
+++ components/MenuCard/MenuCard.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./MenuCard.module.css";
+
+// TODO: estilizar
+type AppProps = {
+  src: string;
+  text: string;
+  alt: string;
+  href: string;
+};
+
+export default function MenuCard({ src, text, alt, href }: AppProps) {
+  return (
+    <Link className={styles["content-container"]} href={href}>
+      <div>
+        <Image
+          className={styles["image"]}
+          src={src}
+          width={223}
+          height={223}
+          alt={alt}
+        />
+        <div className={styles["pseudo"]}></div>
+      </div>
+      <p className={styles["title"]}>{text}</p>
+    </Link>
+  );
+}
