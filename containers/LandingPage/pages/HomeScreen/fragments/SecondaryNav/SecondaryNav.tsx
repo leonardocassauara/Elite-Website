9587c004251698db1e6f7bc01ec7bd0625a8bc5e
Leonardo Cassauara <leomaia.rj@hotmail.com>
2024-05-11 13:18:31 -0300

    feat: criando componente navegação secundária

---
+++ containers/LandingPage/pages/HomeScreen/fragments/SecondaryNav/SecondaryNav.tsx
@@ -0,0 +1,41 @@
+import styles from "./SecondaryNav.module.css";
+import MenuCard from "@/components/MenuCard/MenuCard";
+
+export default function SecondaryNav() {
+  return (
+    <nav className={styles["background-container"]}>
+      <div className={styles["content-container"]}>
+        <MenuCard
+          href="./"
+          text="PNEUS REMOLD"
+          src="/img/second menu.png"
+          alt="Navegação Pneu Remold"
+        />
+        <MenuCard
+          href="./"
+          text="PNEUS DUNLOP"
+          src="/img/second menu.png"
+          alt="Navegação Pneu Dunlop"
+        />
+        <MenuCard
+          href="./"
+          text="PNEUS STRONG"
+          src="/img/second menu.png"
+          alt="Navegação Pneu Strong"
+        />
+        <MenuCard
+          href="./"
+          text="PEÇAS"
+          src="/img/second menu.png"
+          alt="Navegação Peças"
+        />
+        <MenuCard
+          href="./"
+          text="SERVIÇOS"
+          src="/img/second menu.png"
+          alt="Navegação Serviços"
+        />
+      </div>
+    </nav>
+  );
+}
