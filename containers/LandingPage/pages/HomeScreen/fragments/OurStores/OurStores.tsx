564882740e8a0f1e782b55b41fb472c6dce9718f
Leonardo Cassauara <leomaia.rj@hotmail.com>
2024-05-13 11:11:19 -0300

    feat: criando componente nossas lojas

---
+++ containers/LandingPage/pages/HomeScreen/fragments/OurStores/OurStores.tsx
@@ -0,0 +1,44 @@
+import Title from "@/components/Title/Title";
+import styles from "./OurStores.module.css";
+import Image from "next/image";
+
+export default function OurStores() {
+  return (
+    <>
+      <div className={styles["background-container"]}>
+        <div className={styles["title-container"]}>
+          <Title text="NOSSAS UNIDADES" pseudo color="#ffffff" />
+        </div>
+      </div>
+      <div className={styles["background-container"]}>
+        <div className={styles["image-container"]}>
+          <div className={styles["pseudo-container"]}>
+            <span className={styles["pseudo"]}>
+              Av. Areia Branca, 2080 - Santa Cruz, Rio de Janeiro
+            </span>
+            <Image
+              src="/img/stores.png"
+              width={559}
+              height={740}
+              alt="Unidade Areia Branca"
+              className={styles["image"]}
+            />
+          </div>
+
+          <div className={styles["pseudo-container"]}>
+            <span className={styles["pseudo"]}>
+              Av. Padre Guilherme Decaminada, 1170 - Santa Cruz, Rio de Janeiro
+            </span>
+            <Image
+              src="/img/stores.png"
+              width={559}
+              height={740}
+              alt="Unidade Padre Guilherme"
+              className={styles["image"]}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
