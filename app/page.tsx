23fceef8b673edaf45d7bb7fe723e060a102fe89
Leonardo Cassauara <leomaia.rj@hotmail.com>
2024-05-15 17:54:36 -0300

    feat: adicionando meta tag descrição

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
 import HomeScreen from "@/containers/LandingPage/pages/HomeScreen/HomeScreen";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  description:
+    "Qualidade em pneus, autopeças e serviços automotivos. Elite Auto Center há 15 anos, garantindo sua satisfação!",
+};
 
 export default function Home() {
   return (
