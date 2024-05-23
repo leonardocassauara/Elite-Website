78720fc998de57fbfd1835cbfebc7cce27219a5b
Leonardo Cassauara <leomaia.rj@hotmail.com>
2024-05-23 13:03:48 -0300

    fix: corrigindo passagem de children

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -10,33 +10,31 @@ export default function Header() {
     <header className={styles["background-container"]}>
       <div className={styles["main-container"]}>
         <Logo width={60} height={54} priority />
-        <Hamburguer
-          children={
-            <ul className={styles["column-container"]}>
-              <li>
-                <Button text="Pneus" visual="clean" mode="link" href="./" />
-              </li>
-              <li>
-                <Button text="Peças" visual="clean" mode="link" href="./" />
-              </li>
-              <li>
-                <Button text="Serviços" visual="clean" mode="link" href="./" />
-              </li>
-              <li>
-                <Button text="Contato" visual="clean" mode="link" href="./" />
-              </li>
-              <li>
-                <Button
-                  text="Fale com um atendente"
-                  visual="outline"
-                  mode="link"
-                  href="./"
-                  style={{ padding: "0.375rem 0.75rem", whiteSpace: "nowrap" }}
-                />
-              </li>
-            </ul>
-          }
-        />
+        <Hamburguer>
+          <ul className={styles["column-container"]}>
+            <li>
+              <Button text="Pneus" visual="clean" mode="link" href="./" />
+            </li>
+            <li>
+              <Button text="Peças" visual="clean" mode="link" href="./" />
+            </li>
+            <li>
+              <Button text="Serviços" visual="clean" mode="link" href="./" />
+            </li>
+            <li>
+              <Button text="Contato" visual="clean" mode="link" href="./" />
+            </li>
+            <li>
+              <Button
+                text="Fale com um atendente"
+                visual="outline"
+                mode="link"
+                href="./"
+                style={{ padding: "0.375rem 0.75rem", whiteSpace: "nowrap" }}
+              />
+            </li>
+          </ul>
+        </Hamburguer>
         <nav className={styles["nav-container"]}>
           <ul className={styles["row-container"]}>
             <li>
