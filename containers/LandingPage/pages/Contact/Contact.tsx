dcfb12605bcf421a73b88c296eea19eae5a47710
Leonardo Cassauara Maia <leomaia.rj@hotmail.com>
2024-06-02 18:06:02 -0300

    refactor: renomeando fragment e import

--- containers/LandingPage/pages/Contact/Contact.tsx
+++ containers/LandingPage/pages/Contact/Contact.tsx
@@ -1,11 +1,11 @@
 import Banner from "./fragments/Banner/Banner";
-import Maps from "./fragments/Maps/Maps";
+import SupportInfo from "./fragments/SupportInfo/SupportInfo";
 
 export default function Contact() {
   return (
     <>
       <Banner />
-      <Maps />
+      <SupportInfo />
     </>
   );
 }
