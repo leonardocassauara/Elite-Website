1fecfd2e51341a78168c1544e504c1a40c8e3743
Leonardo Cassauara Maia <leomaia.rj@hotmail.com>
2024-06-29 11:09:43 -0300

    fix: tornando o html mais semântico

--- components/ProductCard/ProductCard.tsx
+++ components/ProductCard/ProductCard.tsx
@@ -11,24 +11,22 @@ type AppProps = {
 
 export default function ProductCard({ id, name, price, href }: AppProps) {
   return (
-    <>
-      <div id={id} className={styles["main-container"]}>
-        <Link href={href} className="focusable">
-          <Image
-            src="/img/product.png"
-            width={318}
-            height={345}
-            alt="Produto em destaque"
-            className={styles["image"]}
-          />
-          <div className={styles["text-container"]}>
-            <div className={styles["content-container"]}>
-              <h3 className={styles["title"]}>{name}</h3>
-              <p className={styles["description"]}>R${price}</p>
-            </div>
+    <li id={id} className={styles["main-container"]}>
+      <Link href={href} className="focusable">
+        <Image
+          src="/img/product.png"
+          width={318}
+          height={345}
+          alt="Produto em destaque"
+          className={styles["image"]}
+        />
+        <div className={styles["text-container"]}>
+          <div className={styles["content-container"]}>
+            <h3 className={styles["title"]}>{name}</h3>
+            <p className={styles["description"]}>R${price}</p>
           </div>
-        </Link>
-      </div>
-    </>
+        </div>
+      </Link>
+    </li>
   );
 }
