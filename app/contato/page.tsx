396b314eb543377d7396620dcfd21ae3881e0b5c
leonardocassauara <leomaia.rj@hotmail.com>
2025-01-27 21:33:26 -0300

    feat: adicionando descricao a pagina

--- app/contato/page.tsx
+++ app/contato/page.tsx
@@ -1,4 +1,9 @@
 import Contact from "@/containers/LandingPage/pages/Contact/Contact";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  description: "Entre em contato com a equipe da Elite Auto Center.",
+};
 
 export default function Contato() {
   return <Contact />;
